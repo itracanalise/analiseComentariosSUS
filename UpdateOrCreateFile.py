0bbed759e9349f4c75643736439f865fecebc498
ITRAC UnB <itrac.unb@gmail.com>
2021-10-12 17:14:14 +0000

    ajuste de identação

--- UpdateOrCreateFile.py
+++ UpdateOrCreateFile.py
@@ -14,8 +14,8 @@ def get_blob_content(repo, branch, path_name):
 	return repo.get_git_blob(sha[0])
 
 def update_Data():
-    g = Github("itracanalise"," ")
-	repo = g.get_repo("itracanalise/analiseComentariosSUS")
+        g = Github("itracanalise","")
+        repo = g.get_repo("itracanalise/analiseComentariosSUS")
 
 	all_files = []
 
